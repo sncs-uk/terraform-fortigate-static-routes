984b9cc38c7a6902b6c4ae7a3e55a0905dc51b95
Richard Franks <richard.franks@securenetcoms.co.uk>
2025-11-24 14:06:19 +0000

    Updated versions and formatting

--- main.tf
+++ main.tf
@@ -4,40 +4,42 @@
  * This terraform module configures static routes on a firewall
  */
 terraform {
+  required_version = ">= 1.13.0"
   required_providers {
     fortios = {
       source  = "fortinetdev/fortios"
+      version = ">= 1.22.0"
     }
   }
 }
 locals {
-  vdom_static_yaml  = {
+  vdom_static_yaml = {
     for vdom in var.vdoms : vdom => yamldecode(file("${var.config_path}/${vdom}/static.yaml")) if fileexists("${var.config_path}/${vdom}/static.yaml")
   }
 
-  routes            = flatten([
+  routes = flatten([
     for vdom in var.vdoms : [
-      for route in try(local.vdom_static_yaml[vdom], []) : [ merge(route, { vdom = vdom }) ]
+      for route in try(local.vdom_static_yaml[vdom], []) : [merge(route, { vdom = vdom })]
     ]
   ])
 }
 
-resource fortios_router_static static_route {
-  for_each              = { for route in try(local.routes, []) : "${route.dst}_${route.gateway}" => route}
-  device                = try(each.value.device, null)
-  dst                   = each.value.dst
-  gateway               = each.value.gateway
-  vdomparam             = each.value.vdom
-  bfd                   = try(each.value.bfd, null)
-  blackhole             = try(each.value.blackhole, null)
-  distance              = try(each.value.distance, null)
-  dynamic_gateway       = try(each.value.dynamic_gateway, null)
-  internet_service      = try(each.value.internet_service, null)
-  link_monitor_exempt   = try(each.value.link_monitor_exempt, null)
-  priority              = try(each.value.priority, null)
-  src                   = try(each.value.src, null)
-  status                = try(each.value.status, null)
-  virtual_wan_link      = try(each.value.virtual_wan_link, null)
-  vrf                   = try(each.value.vrf, null)
-  weight                = try(each.value.weight, null)
+resource "fortios_router_static" "static_route" {
+  for_each            = { for route in try(local.routes, []) : "${route.dst}_${route.gateway}" => route }
+  device              = try(each.value.device, null)
+  dst                 = each.value.dst
+  gateway             = each.value.gateway
+  vdomparam           = each.value.vdom
+  bfd                 = try(each.value.bfd, null)
+  blackhole           = try(each.value.blackhole, null)
+  distance            = try(each.value.distance, null)
+  dynamic_gateway     = try(each.value.dynamic_gateway, null)
+  internet_service    = try(each.value.internet_service, null)
+  link_monitor_exempt = try(each.value.link_monitor_exempt, null)
+  priority            = try(each.value.priority, null)
+  src                 = try(each.value.src, null)
+  status              = try(each.value.status, null)
+  virtual_wan_link    = try(each.value.virtual_wan_link, null)
+  vrf                 = try(each.value.vrf, null)
+  weight              = try(each.value.weight, null)
 }
--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
-variable vdoms {
+variable "vdoms" {
   description = "List of VDOMs from which to pull in configuration"
   type        = list(string)
   default     = []
 }
 
-variable config_path {
+variable "config_path" {
   description = "Path to base configuration directory"
   type        = string
 }
